4515613cb34567cd1a94dfe2c90ebae6715d1b0e
Users <lana.chebotareva.95@mail.ru>
2018-08-10 17:03:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/picasopetrofit/MoviesApiService.java
@@ -0,0 +1,15 @@
+package com.example.user.picasopetrofit;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MoviesApiService {
+    @GET("movie/popular")
+    Call<List<Movie>> getPopularMovies(@Query("api_key") String api_key);
+
+}
